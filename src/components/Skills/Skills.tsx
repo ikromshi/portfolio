7ff3a77bdd5fd376a502c219b5d31b92ac8e5404
Ikrom Numonov <numanovikrom454@gmail.com>
2022-09-28 10:15:03 -0400

    centered the items in cols

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,10 +1,25 @@
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { Container, Row, Col } from "react-bootstrap"; 
-import meter1 from "../../assets/image/meter1.svg";
-import meter2 from "../../assets/image/meter2.svg";
-import meter3 from "../../assets/image/meter3.svg";
+import css from "../../assets/skill-logos/css.svg";
+import firebase from "../../assets/skill-logos/firebase.svg";
+import flask from "../../assets/skill-logos/flask.svg";
+import git from "../../assets/skill-logos/git.svg";
+import github from "../../assets/skill-logos/github.svg";
+import graphql from "../../assets/skill-logos/graphql.svg";
+import html from "../../assets/skill-logos/html.svg";
+import java from "../../assets/skill-logos/java.svg";
+import javascript from "../../assets/skill-logos/javascript.svg";
+import python from "../../assets/skill-logos/python.svg";
+import r from "../../assets/skill-logos/r.svg";
+import react from "../../assets/skill-logos/react.svg";
+import redux from "../../assets/skill-logos/redux.svg";
+import sass from "../../assets/skill-logos/sass.svg";
+import sqlalchemy from "../../assets/skill-logos/sqlalchemy.svg";
+import sqlite from "../../assets/skill-logos/sqlite.svg";
+import typescript from "../../assets/skill-logos/typescript.svg";
 import colorSharp from "../../assets/image/color-sharp.png";
+import arrow1 from "../../assets/image/arrow1.svg";
 import "./Skills.css";
 
 export const Skills = () => {
@@ -16,11 +31,11 @@ export const Skills = () => {
   },
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
-    items: 3
+    items: 5
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 2
+    items: 3
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
@@ -36,24 +51,50 @@ export const Skills = () => {
             <div className="skill-bx">
               <h2>Skills</h2>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora, animi amet. Placeat, culpa animi. Est explicabo totam sit perferendis a et iste, amet neque nam, ab, aut similique magni perspiciatis.</p>
-              <Carousel responsive={responsive} infinite={true} className="skill-slider">
-                <div className="item">
-                  <img src={meter1} alt="Img" />
-                  <h5>Web Development</h5>
-                </div>
-                <div className="item">
-                  <img src={meter2} alt="Img" />
-                  <h5>Skill 2</h5>
-                </div>
-                <div className="item">
-                  <img src={meter3} alt="Img" />
-                  <h5>Skill 3</h5>
-                </div>
-                <div className="item">
-                  <img src={meter1} alt="Img" />
-                  <h5>Skill 4</h5>
-                </div>
-              </Carousel>
+              <h4>Languages I am proficient in:</h4>
+              <Row className="skill-slider">
+                <Col className="item">
+                  <img src={typescript} alt="Img" className="img"/>
+                  <h5>TypeScript</h5>
+                </Col>
+                <Col className="item">
+                  <img src={javascript} alt="Img" />
+                  <h5>JavaScript</h5>
+                </Col>
+                <Col className="item">
+                  <img src={java} alt="Img" />
+                  <h5>Java</h5>
+                </Col>
+                <Col className="item">
+                  <img src={python} alt="Img" />
+                  <h5>Python</h5>
+                </Col>
+                <Col className="item">
+                  <img src={r} alt="Img" />
+                  <h5>R</h5>
+                </Col>
+                <Col className="item">
+                  <img src={css} alt="Img" />
+                  <h5>CSS</h5>
+                </Col>
+                <Col className="item">
+                  <img src={html} alt="Img" />
+                  <h5>HTML</h5>
+                </Col>
+                
+              </Row>
+              <h4>Frameworks I commonly use:</h4>
+              <Row className="skill-slider">
+                <Col className="item">
+                  <img src={react} alt="Img" className="img"/>
+                  <h5>React</h5>
+                </Col>
+                <Col className="item">
+                  <img src={flask} alt="Img" />
+                  <h5>Flask</h5>
+                </Col>
+                {/*add empty columns to get the same size*/}
+              </Row>
             </div>
           </Col>
         </Row>
