5457208daf1c24eb79c318d2c342e1147594101f
ikromshi <numanovikrom454@gmail.com>
2023-07-26 12:43:13 -0400

    img overflow fixed

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -45,16 +45,14 @@ const Banner = () => {
         <div className="banner-bg">
           <Row className="align-items-cente">
             <Col xs={12} md={6} xl={7}>
-              {/* <span className="tagline">Welcome to my Portfolio!</span> */}
               <h1>{`Hi, I'm Ikrom. I am a `}<span className="wrap">{text}</span></h1>
               <h3>Welcome to my Web Portfolio!</h3> <br />
               <p>Here you will have a slight glimpse into my professional world. I have created this website to tell you a little bit about myself as well as share some of the work I've done and skills I've acquired as a CS major.</p> <br />
-              
               <a href="https://www.linkedin.com/in/ikromshi/" target="blank">
                 <button>Let's connect <ArrowRightCircle size={25}/></button>
               </a>
             </Col>
-            <Col xs={12} md={6} xl={5}>
+            <Col className="d-none d-md-block" xs={12} md={6} xl={5}>
               <img src={headerImg} alt="Header" className="header-img"/>
             </Col>
           </Row>
